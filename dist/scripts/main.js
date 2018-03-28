fe7342a36f98fa6372932d0e2ab3bb4efdf60af9
GaCon <kaynguyen0904@MacBook-Pro.local>
2018-03-28 14:09:27 +0700

    Update folder dist

---
+++ dist/scripts/main.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";e(document).ready(function(){e("#toggle-menu").on("click",function(){e("#toggle-menu").toggleClass("opened"),e("#main-menu").toggleClass("in"),e(".nav-mobile").toggleClass("out")})})}(jQuery);
+//# sourceMappingURL=main.js.map
