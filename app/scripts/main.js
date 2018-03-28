dce1e879accbe8e222c89a0d5bc279788d601d73
GaCon <kaynguyen0904@MacBook-Pro.local>
2018-03-28 17:07:35 +0700

    Update homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,5 +10,28 @@
       $('.nav-mobile').toggleClass('out')
     })
 
+    $('header .nav-menu a.nav-menu-item').on('click', function() {
+      ($(this).parent().find('a.nav-menu-item').hasClass('active')) ? $(this).parent().find('a.nav-menu-item').removeClass('active') : ''
+      $(this).addClass('active')
+    })
+
+    $(window).scroll(function(event){
+      var bannerHeight = $('.section-banner').outerHeight()
+      if($(this).scrollTop() >= parseInt(bannerHeight)-110){
+        $('.nav-mobile a.nav-mobile-trans-top').addClass('hidden')
+        $('.nav-mobile a.nav-mobile-trans-bot').removeClass('hidden')
+      }
+      if($(this).scrollTop() < parseInt(bannerHeight)-400){
+        $('.nav-mobile a.nav-mobile-trans-top').removeClass('hidden')
+        $('.nav-mobile a.nav-mobile-trans-bot').addClass('hidden')
+      }
+    })
+
+    $('.nav-mobile a.nav-mobile-trans-bot').on('click', function() {
+      $('html, body').animate({
+        scrollTop: $('.section-banner').offset().top-200
+      }, 1000);
+    })
+
   })
 })(jQuery)
